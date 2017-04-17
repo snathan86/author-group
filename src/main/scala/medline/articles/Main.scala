791c335b1a8fbadb2f8df059f43d5c1719ca4356
SenthilNathan <snathan86@gmail.com>
2017-04-17 08:48:50 +0530

    mapper improvement

--- src/main/scala/medline/articles/Main.scala
+++ src/main/scala/medline/articles/Main.scala
@@ -14,24 +14,37 @@ object Main {
     val sc = new SparkContext(conf)
 
     val rdd = sc.parallelize(JsonParser.parseArticles("/home/senthil/medline.json"))
-    //rdd.map( article => article.)
-    val mappedRDD = rdd.flatMap(article => mapAuthors1(article) )
+    val indexedRDD = rdd.flatMap(indexAuthors)
+
+   // val mappedRDD = rdd.flatMap(article => mapAuthors1(article) )
     //rdd.fold(List[String]())(mapAuthors)
-    val result = mappedRDD.map((_, 1)).reduceByKey(_ + _).collect()
-    
-    result.foreach(s => println(s"NAME : ${s._1},  COUNT: ${s._2}"))
-    println("FINAL RESULT MAPPED TOGETHER: "+result)
+    //val result = mappedRDD.map((_, 1)).reduceByKey(_ + _).collect()
+      indexedRDD.foreach(e => println(s"KEY : ${e._1}  INDEX : ${e._2}"))
+    //result.foreach(s => println(s"NAME : ${s._1},  COUNT: ${s._2}"))
+    //println("FINAL RESULT MAPPED TOGETHER: "+result)
     //mappedRDD.groupBy()
+  }
+  //use linked hash map
+  def indexAuthors(article : Article): Map[String,Int] = {
+    val authors =article.AuthorList.Author
+    //authors.map( author => constructKey(author) -> 1 )
+    authors.foldLeft(Map[String,Int]())(index)
+
+  }
+
+  def index(map: Map[String,Int], author : Author) : Map[String,Int] = {
+    map + (constructKey(author) -> map.size)
   }
 
   def mapAuthors1(article : Article) : List[String] ={
     //article.AuthorList.author.foldLeft(List[String]())(foldAuthor)
-    combine(article.AuthorList.Author)
+    build(article.AuthorList.Author,List[String]())
   }
 
   //TODO: do it with match case head::tail something like this in functional style
-  def combine(authors : List[Author]) : List[String] = {
+  def combine2(authors : List[Author]) : List[String] = {
     var lst : List[String] = List()
+
     for(i <- authors.indices) {
       var j = i
       while(j<authors.length) {
@@ -51,6 +64,30 @@ object Main {
     }
     lst
   }
+
+  def build(authors : List[Author],acc : List[String]) : List[String] = {
+   authors match {
+     case head +: Nil => combine(head,head) +: acc
+     case head +: tail => build(tail,combine(head,head) +: tail.map(e => combine(head,e)) ++: acc)
+   }
+  }
+
+  def combine(author1: Author , author2 : Author) : String = {
+
+    val a1 = constructKey(author1)
+    val a2 = constructKey(author2)
+
+    a1<a2 match {
+      case true => s"$a1|$a2"
+      case _ => s"$a2|$a1"
+    }
+  }
+
+  def constructKey(author : Author) : String = {
+      s"${author.LastName}~${author.ForeName}~${author.Initials}"
+  }
+
+
 
   def mapAuthors(article: Article,authors: List[String]) : List[String] = {
     authors :+ article.AuthorList.Author.toString()
--- src/main/scala/senthil/learn/JsonParser.scala
+++ src/main/scala/senthil/learn/JsonParser.scala
@@ -21,7 +21,7 @@ object JsonParser {
   implicit val formats = DefaultFormats // Brings in default date formats etc.
 
   def main(args: Array[String]): Unit = {
-    val lines = scala.io.Source.fromFile("/home/senthil/medline.json").mkString
+   // val lines = scala.io.Source.fromFile("/home/senthil/medline.json").mkString
 
     /** val json = parse("""
       * { "name": "joe",
@@ -43,7 +43,7 @@ object JsonParser {
       * }
       * """)
       * val persons = json.extract[Person] **/
-    val json = parse(lines)
+//    val json = parse(lines)
     val llss = List(Article("Title 1",AuthorList(List(Author("Abraham","Alice","A"), Author("Byron","Bill","B")))),
     Article("Title 2",AuthorList(List(Author("Byron","Bill","B"), Author("Chang","Christie","C")))),
     Article("Title 3",AuthorList(List(Author("Chang","Christie","C"), Author("Abraham","Alice","A")))),
@@ -54,13 +54,63 @@ object JsonParser {
     Article("Title 8 2 author",AuthorList(List(Author("Ram","Laksh","C"), Author("Jayanth","Yadav","V")))),
     Article("Title 9 3 author",AuthorList(List(Author("Christoper","Nolan","W"), Author("Tom","Cruise","K"), Author("Rajni","Kanth","S")))),
       Article("Title 10 3 authors",AuthorList(List(Author("Doel","David","J"), Author("Byron","Bill","B"), Author("Abraham","Alice","A")))))
-
+val lst = List("A","B","C")
+println(build1(lst,List[String]()))
     //val authorList = parseArticles("/home/senthil/medline.json")
-      println(json.extract[List[Article]])
+     // println(json.extract[List[Article]])
    // println(authorList.apply(0).AuthorList.Author)
     //println(Main.combine(authorList.apply(6).AuthorList.Author))
     //println(Main.combine(authorList.apply(7).AuthorList.Author))
 
+  }
+
+  def buildddddd(lst: List[String],op:List[String]) : List[String] = {
+    val s =lst match {
+      case head +: Nil => { val s = combine(head,head)
+      println("first case "+s)
+        val l = op :+ s
+        l
+      }
+      case head +: tail => {
+        val l = op :+ combine(head, head) :+ combine(head,tail.head)
+        println("op here:"+l)
+        buildddddd(tail,l)
+      }
+
+    }
+    s
+  }
+
+  def build1(lst: List[String],op:List[String]) : List[String] = {
+    val s =lst match {
+      case head +: Nil => { combine(head,head) +: op }
+      case head +: tail => {
+        build1(tail,combine(head, head) +: tail.map(ele => combine(head,ele)) ++:  op)
+      }
+    }
+    s
+  }
+
+  def build2(lst: List[String],op:List[String]) : List[String] = {
+    val s =lst match {
+      case head +: Nil => List(head)
+      case head +: tail => {
+       val temp =    tail.map(ele => combine(head,ele)) ++: op :+ combine(head, head)
+        build1(tail,temp)
+      // val temp1 =  combine(head, head) +: combine(head, build1(tail,op).apply(0))
+        //List(tail.head)
+       //combine(head, head) +: combine(head,build1(tail,op).apply(0))
+      }
+    }
+    s
+  }
+
+// tail match { case Nil => Nil
+ // case _ => combine(head, tail.head)}
+ // buildByFold(op:List[String],)
+
+  def combine(a1 : String , a2: String): String = {
+    s"$a1 ~ $a2"
   }
   def parseArticles(filePath : String) : List[Article] = {
     //val lines= scala.io.Source.fromFile(filePath).mkString
@@ -74,6 +124,7 @@ object JsonParser {
       Article("Title 6 1 author",AuthorList(List(Author("Chang","Christie","C")))),
       Article("Title 7 4 author",AuthorList(List(Author("Chang","Christie","C"), Author("Parithi","Muthu","I"), Author("Subbu","Laks","M"), Author("Doel","David","J")))),
       Article("Title 8 2 author",AuthorList(List(Author("Ram","Laksh","C"), Author("Jayanth","Yadav","V")))),
+      Article("Title 11 2 authors",AuthorList(List(Author("Ram","Laksh","C"), Author("Jayanth","Yadav","V")))),
       Article("Title 9 3 author",AuthorList(List(Author("Christoper","Nolan","W"), Author("Tom","Cruise","K"), Author("Rajni","Kanth","S")))),
       Article("Title 10 3 authors",AuthorList(List(Author("Doel","David","J"), Author("Byron","Bill","B"), Author("Abraham","Alice","A")))))
     llss
